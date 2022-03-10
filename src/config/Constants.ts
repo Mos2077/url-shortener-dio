60c9a5e09324d21cb648251e1177d88743eb9624
matheus oliveira silva <matheus_ziikavl2011@hotmail.com>
2022-03-10 15:57:01 -0300

    Adicionar arquivos de projeto.

---
+++ src/config/Constants.ts
@@ -0,0 +1,6 @@
+export const config = {
+    API_URL: 'http://localhost:4000',
+    MONGO_CONNECTION:
+     "mongodb+srv://Mos2077:80645290@url-shortener-dio.gmuax.mongodb.net/url-shortener-dio?retryWrites=true&w=majority"
+}
+    
